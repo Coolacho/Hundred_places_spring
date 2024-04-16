2feef405cbd5e91d077f05cd6d94cb51944ea5a1
Angel Angelov <sofiaangel35@gmail.com>
2024-04-16 14:01:26 +0300

    Initial commit

---
+++ src/main/java/com/example/hundredplaces/HundredPlacesApplication.java
@@ -0,0 +1,13 @@
+package com.example.hundredplaces;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HundredPlacesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HundredPlacesApplication.class, args);
+    }
+
+}
