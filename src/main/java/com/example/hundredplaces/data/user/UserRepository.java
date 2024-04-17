6a1171040f6181a0c679e8f8081a93c668526136
Angel Angelov <sofiaangel35@gmail.com>
2024-04-17 15:56:39 +0300

    Replaced named query

--- src/main/java/com/example/hundredplaces/data/user/UserRepository.java
+++ src/main/java/com/example/hundredplaces/data/user/UserRepository.java
@@ -3,5 +3,5 @@ package com.example.hundredplaces.data.user;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    User findById(long id);
+    User findByEmailAndPassword(String email, String password);
 }
